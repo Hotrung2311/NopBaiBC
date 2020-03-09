00ba26db66fc757a45c94537b9fa6964a209bf0d
Hotrung2311 <trungho2311@gmail.com>
2020-03-09 11:59:22 +0700

    Update js cho bai tap

---
+++ BT_Bien_KieuDL_ToanTu.js
@@ -0,0 +1,34 @@
+// Bài tập 1
+/*
+var VL1 =prompt("Điểm Vật lý");
+var HH1 =prompt("Điểm Hóa học");
+var SH1 =prompt("Điểm Sinh học");
+var DTB1 = (parseInt(VL1) + parseInt(HH1) + parseInt(SH1))/3;
+
+document.getElementById("VL").innerHTML = VL1;
+document.getElementById("HH").innerHTML = HH1;
+document.getElementById("SH").innerHTML = SH1;
+document.getElementById("DTB").innerHTML = DTB1;
+*/
+
+// Bài tập 2
+/*
+var doC = prompt("Độ C");
+var doF = (parseInt(doC)*9)/5 + 32;
+document.getElementById("doC").innerHTML = doC;
+document.getElementById("doF").innerHTML = doF;
+*/
+
+// Bài tập 3
+/*
+var bk1 = prompt("Bán kính hình tròn");
+var dt = (parseInt(bk1) * parseInt(bk1) * 3.14;
+document.getElementById("bk1").innerHTML = bk1;
+document.getElementById("dt").innerHTML = dt;
+ */
+
+// Bài tập 4
+var bk2 = prompt("Bán kính hình tròn");
+var cv = parseInt(bk2)* 3.14;
+document.getElementById("bk2").innerHTML = bk2;
+document.getElementById("cv").innerHTML = cv;
